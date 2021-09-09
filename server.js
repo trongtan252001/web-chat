1c1c7f57ad3661f3e34a55d10d3ddef307a3b61f
trongtan252001 <76251371+trongtan252001@users.noreply.github.com>
2021-09-09 09:03:36 +0700

    mdam

--- server.js
+++ server.js
@@ -59,15 +59,14 @@ function dangNhap(name, password, socket) {
   }
   socket.emit("dang-nhap-that-bai", name);
 }
-function d1(name, password, socket) {
-    for (var index = 0; index < thongTinNguoiDung.length; index++) {
-      if (
-        thongTinNguoiDung[index].name === name &&
-        thongTinNguoiDung[index].password === password
-      ) {
-        socket.emit("dang-nhap-thanh-cong", name);
-        return;
-      }
+// commit 2
+function testGitHub(name, password, socket) {
+  for (var index = 0; index < thongTinNguoiDung.length; index++) {
+    if (thongTinNguoiDung[index].name === name) {
+      socket.emit("dang-ky-that-bai", name);
+      return;
     }
-    socket.emit("dang-nhap-that-bai", name);
   }
+  thongTinNguoiDung.push(new nguoiDung(name, password));
+  socket.emit("dang-ky-thanh-cong", name);
+}
