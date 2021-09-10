15aeda4d419f59fe8e9cdd034125b6a88c32ba9f
quanglinh2909 <69236990+quanglinh2909@users.noreply.github.com>
2021-09-10 18:18:49 +0700

    linh viet chuc nang gui loi moi ket ban

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ thongTinNguoiDung.push(new nguoiDung("h12", "123"));
 thongTinNguoiDung.push(new nguoiDung("h123", "123"));
 thongTinNguoiDung.push(new nguoiDung("Hau", "123"));
 io.on("connection", (socket) => {
+  socket.on('user-online',username =>{
+      userOnline(username,socket);
+   });
   socket.on("dangKy", (data) => {
     dangKy(data.name, data.password, socket);
   });
@@ -37,7 +40,47 @@ io.on("connection", (socket) => {
   socket.on("search", (data) => {
     searchFriend(data, socket);
   });
+  socket.on('add-friend',data =>{
+    friendRequest(data,socket);
+  });
 });
+
+function friendRequest(data,socket) {
+  var name = data.nameFiend;
+  for (let index = 0; index < thongTinNguoiDung.length; index++) {
+     if(thongTinNguoiDung[index].name === name && !checkFriendRequest(name,data.user)){
+      thongTinNguoiDung[index].arrayFriendRequest.push({data:data,status:true});
+      io.to(thongTinNguoiDung[index].id).emit('notify-request-friend',thongTinNguoiDung[index].arrayFriendRequest);
+      return;
+     }
+  }
+  
+}
+function checkFriendRequest(name,user) {
+  for (let index = 0; index < thongTinNguoiDung.length; index++) {
+    if(thongTinNguoiDung[index].name === name){
+      var array = thongTinNguoiDung[index].arrayFriendRequest;
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(element.data.user === user){
+          return true;
+        }
+      }
+     
+    }
+ }
+ return false;
+}
+function userOnline(name,socket) {
+  for (let index = 0; index < thongTinNguoiDung.length; index++) {
+    if(thongTinNguoiDung[index].name === name){
+     thongTinNguoiDung[index].id = socket.id;
+     socket.emit('notify-request-friend',thongTinNguoiDung[index].arrayFriendRequest);
+
+     return;
+    }
+ }
+}
 function searchFriend(name, socket) {
   var arr = [];
   for (let index = 0; index < thongTinNguoiDung.length; index++) {
@@ -51,6 +94,8 @@ function searchFriend(name, socket) {
 function nguoiDung(name, password) {
   this.name = name;
   this.password = password;
+  this.id = "";
+  this.arrayFriendRequest = [];
 }
 function dangKy(name, password, socket) {
   for (var index = 0; index < thongTinNguoiDung.length; index++) {
